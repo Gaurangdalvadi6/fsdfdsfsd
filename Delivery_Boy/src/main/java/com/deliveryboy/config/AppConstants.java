b244c80ef3abba4d4d018d43dca0d6c26bd67d4e
gaurangdalvadi6 <gaurangdalvadi6@gmail.com>
2024-06-28 21:09:22 +0530

    apache kafka basic producer and consumer completed

---
+++ Delivery_Boy/src/main/java/com/deliveryboy/config/AppConstants.java
@@ -0,0 +1,9 @@
+package com.deliveryboy.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConstants {
+
+	public static final String LOCATION_TOPIC_NAME = "location_update_topic";
+}
---
+++ Enduser/src/main/java/com/enduser/config/AppConstants.java
@@ -0,0 +1,11 @@
+package com.enduser.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConstants {
+
+	public static final String LOCATION_UPDATE_TOPIC="location_update_topic";
+	
+	public static final String GROUP_ID="group-1";
+}
